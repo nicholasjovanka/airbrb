a8aa9348fc57e5c8af46cd955c012e08975e1aaa
eggywrhmn <eggy910@gmail.com>
2023-11-11 13:55:45 +1100

    2.1 Login and Register

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { StoreContext } from '../../utils/states';
+import { apiCall } from '../../utils/utils';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(''); // State to store error messages
+  const navigate = useNavigate();
+  const { loggedIn } = useContext(StoreContext);
+
+  const handleLogin = async () => {
+    try {
+      const response = await apiCall('user/auth/login', 'POST', {
+        email: email,
+        password: password
+      });
+
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('userEmail', email);
+
+      loggedIn[1](true);
+
+      // Redirect the user to the home page
+      navigate('/home');
+    } catch (error) {
+      console.error('Login Error:', error.response?.data?.error || 'Unknown error');
+      setError(error.response?.data?.error || 'An unexpected error occurred.');
+    }
+  };
+
   return (
-     <Box>
-        Welcome to login
-     </Box>
-  )
-}
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p className="error">{error}</p>} {/* Display the error message */}
+    </div>
+  );
+};
 
 export default Login;
--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { StoreContext } from '../../utils/states';
+import { apiCall } from '../../utils/utils';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const { loggedIn } = useContext(StoreContext);
+
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      setError('Passwords do not match.');
+      return;
+    }
+
+    try {
+      const response = await apiCall('user/auth/register', 'POST', {
+        email,
+        name,
+        password,
+      });
+
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('userEmail', email);
+        loggedIn[1](true);
+        navigate('/home');
+      } else {
+        setError(response.data.message || 'Registration failed.');
+      }
+    } catch (error) {
+      setError(error.response?.data?.error || 'An unexpected error occurred.');
+    }
+  };
+
   return (
-     <Box>
-        Welcome to Register
-     </Box>
-  )
-}
+    <div>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password" />
+      <button onClick={handleRegister}>Register</button>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
 
 export default Register;
